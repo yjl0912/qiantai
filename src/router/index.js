d5a79595d1085a09b2678d6aeb0ba81aba8f609e
yjl0912 <2382813371@qq.com>
2020-12-09 17:09:42 +0800

    feat:路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,23 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '../store'
 
-import Home from '../views/Home'
-import Login from '../views/Login'
-import Register from '../views/Register'
-import Search from '../views/Search'
-import Detail from '@views/Detail'
-import AddCartSuccess from "../views/AddCartSuccess";
-import ShopCart from "../views/ShopCart";
-import Trade from "../views/Trade";
-import Pay from "../views/Pay";
-import PaySuccess from "../views/PaySuccess";
-import Center from "../views/Center";
+
+const  Home = ()=>import('../views/Home')
+const  Login = ()=>import('../views/Login')
+const  Register = ()=>import('../views/Register')
+const  Search = ()=>import('../views/Search')
+const Detail = ()=>import('@views/Detail')
+const  AddCartSuccess = ()=>import("../views/AddCartSuccess")
+const  ShopCart = ()=>import("../views/ShopCart")
+const Trade = ()=>import("../views/Trade")
+const  Pay = ()=>import("../views/Pay")
+const PaySuccess = ()=>import("../views/PaySuccess")
+const  Center = ()=>import("../views/Center")
+
+
+
+
+
 
 Vue.use(VueRouter)
 const router = new VueRouter ({
